8098b712eac65ebc835fbf0324ccfd7631631a31
Umut Civici <36381191+umutcvc@users.noreply.github.com>
2022-01-12 11:06:02 -0500

    Add files via upload

---
+++ 02_Software/sketch_jan11a.ino
@@ -0,0 +1,25 @@
+// Define pin connections & motor's steps per revolution
+const int dirPin = 2;
+const int stepPin = 3;
+const int stepsPerRevolution = 200;
+
+void setup()
+{
+  // Declare pins as Outputs
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+}
+void loop()
+{
+  // Set motor direction clockwise
+  digitalWrite(dirPin, HIGH);
+
+  // Spin motor slowly
+  for(int x = 0; x < stepsPerRevolution; x++)
+  {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(600);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(600);
+  }
+}
